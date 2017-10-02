2716361938375c86edaefab879a8d4522eb247a3
Schumi <s.mihara@fout.jp>
2017-10-02 21:08:33 +0900

    futurize urlparse

--- redquery/__init__.py
+++ redquery/__init__.py
@@ -1,4 +1,7 @@
-from urlparse import urljoin
+from future import standard_library
+standard_library.install_aliases()
+from builtins import object
+from urllib.parse import urljoin
 import json
 import time
 
